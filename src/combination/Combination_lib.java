809274c730547df9d136ab75502112ba0444c246
50616017 <50616017@50616017-pc>
2017-10-27 13:19:49 +0900

    combination

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,20 @@
+package combination;
+
+public class Combination_lib {
+	
+	
+	public int getFactorial(int fact) {
+		int r = 1;
+		//ŠKæŒvZ
+		for (int i = 1; i <= fact; i++) {
+			r = r*i;
+			
+		}
+		
+		return r;
+	
+	}
+	
+	
+
+}
